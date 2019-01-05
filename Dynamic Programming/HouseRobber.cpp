81dd25ab08813d78f5991b18a09bf968fa4b85e3
feipxyz <feipxyz@163.com>
2019-01-05 11:34:28 +0800

    打家劫舍

--- Dynamic Programming/HouseRobber.cpp
+++ Dynamic Programming/HouseRobber.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int nMax = 0;
-        if (nums.empty())
-            return 0;
+        if (nums.empty()) return 0;
         vector<int> dp(nums.size(), 0);
         for (int i = 0; i < nums.size(); i++)
         {
             if (i == 0)
-            {
                 dp[i] = nums[i];
-            }
             else if (i == 1)
-            {
-                dp[i] = max(dp[i - 1], nums[i]);
-            }
+                dp[i] = max(nums[i], nums[i - 1]);
             else
-            {
                 dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
-            }
-            nMax = max(nMax, dp[i]);
         }
+        return dp[nums.size() - 1];
+    }
+};
 
-        return nMax;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int last1 = 0;
+        int last2 = 0;
+        int cur = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            cur = max(nums[i] + last2, last1);
+            last2 = last1;
+            last1 = cur;
+        }
+        return cur;
     }
 };
