f8be15911efa37775d694d4ca99bbaf9f98f89d9
feipxyz <feipxyz@163.com>
2019-01-16 21:49:13 +0800

    丑数

--- Math/UglyNumber.cpp
+++ Math/UglyNumber.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isUgly(int num) {
-        vector<int> divisor{2, 3, 5};
+        if (num == 0)
+            return false;
 
-        for (auto a : divisor)
-        {
-            if (num == 0)
-                break;
-            while (num % a == 0)
-            {
-                num /= a;
-            }
-        }
+        while (num % 2 == 0)
+            num /= 2;
+        while (num % 3 == 0)
+            num /=3;
+        while (num % 5 == 0)
+            num /= 5;
         return num == 1;
     }
 };
